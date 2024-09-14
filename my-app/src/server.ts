1b849b4634b0137640892499189dbe55fadc04ba
YusukeMizutani11 <yusuke3rdbee@gmail.com>
2024-09-14 00:46:11 -0500

    server setup

--- my-app/src/server.ts
+++ my-app/src/server.ts
@@ -1,17 +1,21 @@
-import express from 'express';
+import express, {Express} from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
+
+const app: Express = express();
 const httpServer = createServer(app);
+const PORT = process.env.PORT;
 const io = new Server(httpServer);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-httpServer.listen(3000, () => {
-  console.log('Server is running on port 3000');
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 io.on('connection', (socket) => {
